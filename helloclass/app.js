6f20208eeb914930459baba75ad6cee4575f2735
phil iverson <piversonp@gmail.com>
2018-11-29 12:01:59 -0700

    DailyPlanet

---
+++ helloclass/app.js
@@ -0,0 +1,7 @@
+var age = 21;
+var country = "USA"
+
+if (true) {
+    console.log 
+    
+}
--- projects/daily-planet-editor/paper.js
+++ projects/daily-planet-editor/paper.js
@@ -9,41 +9,42 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var enemies=["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
 
-function whoWins(isThereKryptonite,enemyName)   
-    if (isThereKryptonite = false) {
+function whoWins(isThereKryptonite, enemyName)  {
+     if (!isThereKryptonite) {
         return "Superman beats " + enemyName + ", of course";    
-
-}   else if (isThereKryptonite = true) {
-        return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
+    }
+        else {
+        return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
+    }
 }
-
-for (var i = 0; i < enemies.length; i++);
-
+for (var i = 0; i < enemies.length; i++) {
     var isThereKryptonite;   
-    if (i === 0)   {
+    if (i%2 === 0)   {
         isThereKryptonite = true;  
-}   else if (i > 0)   { 
+    } else { 
          isThereKryptonite = false;
-}    
+    }    
     console.log(whoWins(isThereKryptonite,enemies[i]));
-
-function howAttractedIsLoisLaneToMe ()
-{
+}
+function howAttractedIsLoisLaneToMe () {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Mat.random()*10)+1);
+    return Math.floor((Math.random()*10)+1);
 }
 
-consol.log( howAttractedIsLoisLaneToMe ()) ;
+console.log( howAttractedIsLoisLaneToMe()) ;
 
 var clarkKent = true;
 var superman = false;
 
-while (clarkKent){
+while (clarkKent) {
     console.log("I'm just a nerdy columnist");
     var phoneBoothQuickChange = Math.random();
     if (phoneBoothQuickChange >= 0.5) {
         clarkKent = false;
         superman = true;
-        console.log("Now I'm Superman!");}}
+        console.log("Now I'm Superman!");
+    }
+}
+
